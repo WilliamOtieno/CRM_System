c4296e99460a612b46427897746a8f512e82f37a
WilliamOtieno <jimmywilliamotieno@gmail.com>
2021-05-16 17:31:39 +0300

    heroku

--- Will_CRM/settings.py
+++ Will_CRM/settings.py
@@ -132,7 +132,7 @@ STATIC_ROOT = "static_root"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 AUTH_USER_MODEL = 'leads.User'
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 LOGIN_REDIRECT_URL = "/leads"
 LOGIN_URL = "/login"
 LOGOUT_REDIRECT_URL = "/"
@@ -178,3 +178,7 @@ LOGGING = {
 }
 
 TAILWIND_APP_NAME = 'theme'
+
+import dj_database_url
+prod_db = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
--- agents/views.py
+++ agents/views.py
@@ -36,7 +36,7 @@ class AgentCreateView(OrganisorAndLoginRequiredMixin, generic.CreateView):
         )
         send_mail(
             subject="You are invited to be an agent",
-            message="You were added as an agent on DJCRM. Please come login to start working.",
+            message="You were added as an agent on Will_CRM. Please come login to start working.",
             from_email="admin@test.com",
             recipient_list=[user.email]
         )
